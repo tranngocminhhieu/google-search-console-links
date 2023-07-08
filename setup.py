704f92f1cd94cca7ebb6295abb2f95868c75721c
Tran Ngoc Minh Hieu <tnmhieu@gmail.com>
2023-07-08 20:16:26 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from setuptools import setup
+
+README = (Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name='gsclinks',
+    version='1.0.1',
+    description='Scrape backlink data from Google Search Console',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/tranngocminhhieu/google-search-console-links',
+    author='Tran Ngoc Minh Hieu',
+    author_email='tnmhieu@gmail.com',
+    packages=['gsclinks'],
+    install_requires=[
+        'beautifulsoup4',
+        'requests',
+        'tqdm'
+    ]
+)
